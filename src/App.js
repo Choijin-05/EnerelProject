f21528e496ffb4c876e9ebd745b4bdc0c14b7a30
Choijin-05 <a.choijin05@gmail.com>
2025-02-18 23:14:54 +0800

    edit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Энэрэл <code>эмлэлгийн</code> веб сайт.
+          Энэрэл <code>эмнэлгийн </code> веб сайт.
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Эмнэлгийн холбоос
         </a>
       </header>
     </div>
