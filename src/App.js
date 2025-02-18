594d03716a1a57ab4ce3552f9d343a4997418d02
Choijin-05 <a.choijin05@gmail.com>
2025-02-18 23:16:13 +0800

    edit 2

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Эмнэлгийн холбоос
+          Эмнэлгийн холбоос 
         </a>
       </header>
     </div>
